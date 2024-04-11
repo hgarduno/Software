00a8c2e4fe75eb06e2f2a3a4eebdf435bea30cff
Heli Garduno <hgarduno@gmail.com>
2024-04-11 20:42:32 +0000

    Se eliminaron los temporales

--- SiscomElectronica/SQL/ActualizaKitFuenteVariable.sql
+++
@@ -1,2 +0,0 @@
-insert into definiciontiposproducto values (1694258312,'TORNILLO1/2',8);
-insert into definiciontiposproducto values (1694258312,'TUERCAMETAL1/8',8);
--- SiscomElectronica/SQL/PonACerosCapacitoresElectroliticosBodegaIPN.sql
+++
@@ -1,8 +0,0 @@
-
-update materialbodega set existencia=0 
-where idbodega=0 and cveproducto in(
-				    select cveproducto
-				    from productos 
-				    where cveproducto like 'CE%' and 
-				      dscproducto like '%Electrolitico%');
-  
