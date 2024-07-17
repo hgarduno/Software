8f26e9611239c233fee7d5160599992ca01c30d0
Heli Garduno <hgarduno@gmail.com>
2024-07-17 14:57:34 +0000

    se agrego ...

---
+++ SiscomElectronica/Servidor/H/SqlCorteCajaSucursal.h
@@ -0,0 +1,33 @@
+
+#ifndef __SQLCORTECAJA_H__
+#define __SQLCORTECAJA_H__
+
+#include <SiscomDesarrollo4/H/SiscomOperaciones.h>
+
+int SqlDatosCierreCorteSucursal(SiscomOperaciones *pSisOpePtrDato);
+int SqlEnviaRegistroCambioCaja(SiscomOperaciones *pSisOpePtrDato);
+
+int SqlRegistraCambioCaja(SiscomOperaciones *pSisOpePtrDato);
+int SqlActualizaCambioCaja(SiscomOperaciones *pSisOpePtrDato);
+int SqlCajaConCambio(SiscomOperaciones *pSisOpePtrDato,char *pchrPtrIdCajaCambio);
+int SqlCambiosCaja(SiscomOperaciones *pSisOpePtrDato);
+int SqlCambioCaja(SiscomOperaciones *pSisOpePtrDato);
+
+void InsertIntoCambioCaja(SiscomOperaciones *pSisOpePtrDato,
+                          SiscomRegistroProL *pSisRegProLPtrDato,
+                          char *pchrPtrSql);
+
+void InsertIntoDenominacionesCambioCaja(SiscomOperaciones *pSisOpePtrDato,
+                          		SiscomRegistroProL *pSisRegProLPtrDato,
+                          		char *pchrPtrSql);
+
+
+void UpdateCambioCaja(SiscomOperaciones *pSisOpePtrDato,
+			     SiscomRegistroProL *pSisRegProLPtrDato,
+			     char *pchrPtrSql);
+
+void UpdateDenominacionesCambioCaja(SiscomOperaciones *pSisOpePtrDato,
+			     SiscomRegistroProL *pSisRegProLPtrDato,
+			     char *pchrPtrSql);
+#endif
+
